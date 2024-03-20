fae537f9b7b7fb4e120f88b94110c4fa2189c490
sangho <keumm9635@gmail.com>
2024-03-20 12:37:12 +0900

    fix: 사진 기능 추가

--- src/app/create-challenge/page.tsx
+++ src/app/create-challenge/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { DateTime } from "luxon";
+import { useState } from "react";
+import { useRef } from "react";
+import Link from "next/link";
+
+import Image from "next/image";
+
 import {
   frequencyArr,
   periodArr,
-  useOneWeek,
-  days,
-} from "@/components/createChallenge/createCalendar";
-import CreateChallengeBar from "@/components/common/Create-ChallengeBar";
-import React, { useState } from "react";
+} from "@/components/createChallenge/noCreateCalendar";
 
 import { useCreateMutation } from "@/components/hooks/useChallengeMutation";
 type FrequencyIds = 1 | 2 | 3 | 4 | 5 | 6 | 7;
@@ -22,27 +25,32 @@ type FrequencyChallenge = {
 type PeriodChallenge = {
   id: FrequencyIds;
   value: string;
+  number: number;
 };
 
 const CreateChallengePage = () => {
+  const imgFalseRef = useRef<HTMLImageElement>(null);
+  const imgTrueRef = useRef<HTMLImageElement>(null);
+  const dt = DateTime.now();
   const [title, setTitle] = useState("");
-  // const [public, setPublic] = useState(false);
-  const [authorize, setAuthorize] = useState("");
+  const [textAreaAuthorize, setTextAreaAuthorize] = useState("");
   const [frequency, setFrequency] = useState("매일");
   const [period, setPeriod] = useState("");
+  const [periodNum, setPeriodNum] = useState(0);
+  const [startDate, setStartDate] = useState("");
+  const [startToday, setStartToday] = useState("");
+  const [monthWeek, setMonthWeek] = useState(9);
+  const [exampleFalseImg, setExampleFalseImg] = useState(
+    "https://newsimg-hams.hankookilbo.com/2023/06/09/3a4636e2-63f3-4aba-af1e-8f08e469f12d.jpg"
+  );
+  const [exampleTrueImg, setExampleTrueImg] = useState(
+    "https://image.news1.kr/system/photos/2022/3/21/5278974/article.jpg/dims/optimize"
+  );
+  const [img, setImg] = useState("");
 
   const postChallengeMutation = useCreateMutation();
+  const arr = Array.from({ length: 7 }, (v, i) => i++);
 
-  function getDayOfWeek() {
-    const dayOfWeek = new Date().getDay();
-
-    //0:일, 1:월, 2:화, 3:수, 4:목, 5:금, 6:토
-    return dayOfWeek;
-  }
-  console.log(getDayOfWeek());
-
-  const toUseOneWeek = useOneWeek();
-  console.log(toUseOneWeek);
   const frequencyFunc = (id: FrequencyIds) => {
     const frequencyArrFilter = frequencyArr.find(
       (item) => item.id === id
@@ -54,20 +62,64 @@ const CreateChallengePage = () => {
     const periodArrFilter = periodArr.find(
       (item) => item.id === id
     ) as PeriodChallenge;
+    setPeriodNum(periodArrFilter.number);
+    setMonthWeek(9);
     setPeriod(periodArrFilter.value);
   };
 
+  const onclickStartDay = (idx: number) => {
+    setMonthWeek(idx);
+    const plusDt = dt.plus({ days: idx + periodNum });
+    const todayDt = dt.plus({ days: idx });
+    const laterDate = plusDt.month + "월" + plusDt.day + "일";
+    setStartToday(`${todayDt.month}월${todayDt.day}일`);
+    setStartDate(laterDate);
+  };
+
+  const readFalseImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length) return;
+
+    const imageFile = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      if (!event || !event.target) return;
+      if (typeof event.target.result !== "string" || !imgFalseRef.current)
+        return;
+
+      imgFalseRef.current.src = event.target.result as string;
+    };
+
+    reader.readAsDataURL(imageFile);
+  };
+
+  const readTrueImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.length) return;
+
+    const imageFile = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      if (!event || !event.target) return;
+      if (typeof event.target.result !== "string" || !imgTrueRef.current)
+        return;
+
+      imgTrueRef.current.src = event.target.result as string;
+    };
+
+    reader.readAsDataURL(imageFile);
+  };
   const createChallengeBtn = () => {
     setTitle("");
-    setAuthorize("");
+    setTextAreaAuthorize("");
     postChallengeMutation.mutate();
   };
 
   return (
-    <div className="mx-auto">
+    <div className="mt-8">
       {" "}
-      <div className="text-center">
-        <div>
+      <div className="ml-20">
+        <div className="flex mb-5">
           <h1>제목:&nbsp;</h1>
           <input
             className="border border-black-700 rounded border-black"
@@ -76,14 +128,13 @@ const CreateChallengePage = () => {
             onChange={(e) => setTitle(e.target.value)}
           />
         </div>
-        <h1>인증 빈도:&nbsp;</h1>
-
-        <div className="mt-1 flex">
+        <h1 className="mb-5">인증 빈도:&nbsp;</h1>
+        <div className="mb-5 mt-1 flex">
           {frequencyArr.map((item) => {
             return (
               <div key={item.id}>
                 <button
-                  className={`${
+                  className={`mr-2 ${
                     frequency === item.value ? ACTIVE_BUTTON : INACTIVE_BUTTON
                   }`}
                   onClick={() => frequencyFunc(item.id)}
@@ -94,7 +145,7 @@ const CreateChallengePage = () => {
             );
           })}
         </div>
-        <div>
+        <div className="mb-3">
           {frequencyArr.map((item) => {
             return (
               <div key={item.id}>
@@ -107,13 +158,13 @@ const CreateChallengePage = () => {
             );
           })}
         </div>
-        <h1>챌린지 기간:&nbsp;</h1>
-        <div className="mt-1 flex">
+        <h1 className="mb-3">챌린지 기간:&nbsp;</h1>
+        <div className="mb-3 mt-1 flex">
           {periodArr.map((item) => {
             return (
               <div key={item.id}>
                 <button
-                  className={`${
+                  className={`mr-2 ${
                     period === item.value ? ACTIVE_BUTTON : INACTIVE_BUTTON
                   }`}
                   onClick={() => periodFunc(item.id)}
@@ -124,28 +175,99 @@ const CreateChallengePage = () => {
             );
           })}
         </div>
-        <div>인증 가능 시간</div>
+        {/* <div>인증 가능 시간</div>
         <div className="h-20 w-44 border border-black-700 rounded border-black">
           <button>시작 시간&nbsp;|</button>
           <button>&nbsp;종료 시간</button>
-        </div>
-        <div>
-          <h1>시작일:&nbsp;</h1>
-
-          {toUseOneWeek.map((day, idx) => {
-            return <button key={idx}>{day.slice(6)} ()</button>;
+        </div> */}
+        <h1 className="mb-3">시작일:&nbsp;</h1>
+        <div className="mb-3 mt-1 flex">
+          {arr.map((num) => {
+            const nowMonthWeek = `${dt.month}월 ${dt.day + num}일`;
+            return (
+              <button
+                className={`mr-2 ${
+                  monthWeek === num ? ACTIVE_BUTTON : INACTIVE_BUTTON
+                }`}
+                key={num}
+                onClick={() => onclickStartDay(num)}
+              >
+                {nowMonthWeek}
+              </button>
+            );
           })}
         </div>
+        <h1 className="mb-3">
+          {startDate !== "" && monthWeek !== 9
+            ? `${startToday} ~ ${startDate}`
+            : ""}
+        </h1>
         <div>
-          <div>인증 방법</div>
+          <div className="mb-3">인증 방법</div>
           <textarea
-            value={authorize}
+            className="h-28 w-6/12 border border-black-700 rounded border-black"
+            value={textAreaAuthorize}
+            placeholder="예) 오늘 날짜와 걸음 수가 적힌 만보기 캡쳐 화면 업로드"
             required
-            onChange={(e) => setAuthorize(e.target.value)}
+            onChange={(e) => setTextAreaAuthorize(e.target.value)}
           />
         </div>
-        <button onClick={() => {}}>이전</button>
-        <CreateChallengeBar />
+
+        <label>나쁜 예시 사진</label>
+        <div>
+          <input
+            type="file"
+            onChange={(e) => readFalseImage(e)}
+            accept="image/*"
+          />
+
+          <div>
+            <img
+              ref={imgFalseRef}
+              src={exampleFalseImg}
+              width={200}
+              height={200}
+              alt="img"
+            />
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="file"
+            onChange={(e) => readTrueImage(e)}
+            accept="image/*"
+          />
+
+          <div>
+            <img
+              ref={imgTrueRef}
+              src={exampleTrueImg}
+              width={200}
+              height={200}
+              alt="img"
+            />
+          </div>
+        </div>
+
+        <h1 className="mt-5 mb-5">진짜 인증 사진</h1>
+        <img width={150} height={100} src={img} alt="진짜 이미지" />
+        <input type="file" />
+        <div>
+          <Link
+            className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+            href="/"
+          >
+            이전
+          </Link>
+          <Link
+            className="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+            href="/"
+            onClick={createChallengeBtn}
+          >
+            완료
+          </Link>
+        </div>
       </div>
     </div>
   );
@@ -154,6 +276,6 @@ const CreateChallengePage = () => {
 const ACTIVE_BUTTON =
   "mt-1 flex text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700";
 const INACTIVE_BUTTON =
-  "mt-1 flex border border-black-700 rounded border-black";
+  "mt-1 w-20 h-7 justify-center flex border border-black-700 rounded border-black";
 
 export default CreateChallengePage;
---
+++ src/components/createChallenge/noCreateCalendar.ts
@@ -0,0 +1,83 @@
+import { DateTime } from "luxon";
+
+type FrequencyIds = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+type Days = {
+  id: number;
+  name: string;
+};
+
+type FrequencyChallenge = {
+  id: FrequencyIds;
+  certificationDays: string;
+  value: string;
+  days: number;
+};
+
+type PeriodChallenge = {
+  id: FrequencyIds;
+  value: string;
+  number: number;
+};
+
+const days: Days[] = [
+  { id: 1, name: "월" },
+  { id: 2, name: "화" },
+  { id: 3, name: "수" },
+  { id: 4, name: "목" },
+  { id: 5, name: "금" },
+  { id: 6, name: "토" },
+  { id: 7, name: "일" },
+];
+
+const frequencyArr: FrequencyChallenge[] = [
+  {
+    id: 1,
+    certificationDays: "월, 화, 수, 목, 금, 토, 일 입니다.",
+    value: "매일",
+    days: 7,
+  },
+  {
+    id: 2,
+    certificationDays: "월, 화, 수, 목, 금 입니다.",
+    value: "평일 매일",
+    days: 7,
+  },
+  { id: 3, certificationDays: "토, 일 입니다.", value: "주말 매일", days: 2 },
+  {
+    id: 4,
+    certificationDays: "월, 화, 수, 목, 금, 토, 일 중에 1일 인증해 주세요",
+    value: "주 1일",
+    days: 1,
+  },
+  {
+    id: 5,
+    certificationDays: "월, 화, 수, 목, 금, 토, 일 중에 2일 인증해 주세요",
+    value: "주 2일",
+    days: 2,
+  },
+  {
+    id: 6,
+    certificationDays: "월, 화, 수, 목, 금, 토, 일 중에 3일 인증해 주세요",
+    value: "주 3일",
+    days: 3,
+  },
+  {
+    id: 7,
+    certificationDays: "월, 화, 수, 목, 금, 토, 일 중에 4일 인증해 주세요",
+    value: "주 4일",
+    days: 4,
+  },
+];
+
+const periodArr: PeriodChallenge[] = [
+  { id: 1, value: "1주 동안", number: 7 },
+  { id: 2, value: "2주 동안", number: 14 },
+  { id: 3, value: "3주 동안", number: 21 },
+  { id: 4, value: "4주 동안", number: 28 },
+  { id: 5, value: "5주 동안", number: 35 },
+  { id: 6, value: "6주 동안", number: 42 },
+  { id: 7, value: "7주 동안", number: 49 },
+];
+
+export { frequencyArr, periodArr };
