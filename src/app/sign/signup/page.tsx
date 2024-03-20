787d992020f3d83d552383881b69c866a7a88480
Dahyeon Jin <wlsekgus12@gmail.com>
2024-03-20 15:58:54 +0900

    Feat: 회원가입 기능 구현 완료

--- src/app/sign/signup/page.tsx
+++ src/app/sign/signup/page.tsx
@@ -1,57 +1,71 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useSignup } from '../../../components/hooks/useAuth';
-import { redirect } from 'next/navigation';
+import { supabase } from '@/utils/supabaseClient';
+import { useRouter } from 'next/navigation';
 
 const SignUpPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const signupMutation = useSignup();
+  const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    signupMutation.mutate(
-      { email, password },
-      {
-        onSuccess: () => {
-          alert('회원가입 되셨습니다!');
-          redirect('/sign/signin');
-        },
-        onError: (error) => {
-          alert(`회원가입에 실패하였습니다. ${error.message}`);
-        },
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      if (error) {
+        alert('회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.');
+        console.log(error);
+
+        return;
       }
-    );
+    } catch (error) {
+      alert('회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.');
+      console.log(error);
+    }
+
+    alert('회원가입이 완료되었습니다!');
+
+    // 회원가입 완료 후 로그인으로!!
+    router.replace('/sign/signin');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor='email'>Email:</label>
-        <input
-          id='email'
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password:</label>
-        <input
-          id='password'
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <button type='submit'>회원가입하기</button>
-      </div>
-    </form>
+    <div>
+      <h2>회원가입</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='email'>Email:</label>
+          <input
+            id='email'
+            type='email'
+            value={email}
+            placeholder='ex. changedays@gmail.com'
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password:</label>
+          <input
+            id='password'
+            type='password'
+            value={password}
+            placeholder='ex. changedays!1234'
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type='submit'>회원가입하기</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
