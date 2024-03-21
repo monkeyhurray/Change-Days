6ddf7833ad05cd62910736aa16b7dabdf7fb5cf3
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-03-21 14:23:46 +0900

    temp

--- src/app/sign/signup/page.tsx
+++ src/app/sign/signup/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // [경로 변경 필요] utils 말고 supabase 폴더에서 import
-import { supabase } from '@/utils/supabaseClient';
-import { useRouter } from 'next/navigation';
+import { supabase } from "@/supabase/supabase";
+import { useRouter } from "next/navigation";
 
 const SignUpPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleSubmitSignUp = async (e: React.FormEvent) => {
@@ -19,21 +19,21 @@ const SignUpPage = () => {
         password,
       });
 
-      console.log('data: ', data);
+      console.log("data: ", data);
 
       if (error) {
-        alert('회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.');
+        alert("회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.");
         console.log(error);
 
         return;
       }
     } catch (error) {
-      alert('회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.');
+      alert("회원가입 도중 오류가 발생하였습니다. 고객센터로 연락해주세요.");
       console.log(error);
     }
 
-    alert('회원가입이 완료되었습니다!');
-    router.replace('/sign/signin');
+    alert("회원가입이 완료되었습니다!");
+    router.replace("/sign/signin");
   };
 
   return (
@@ -41,29 +41,29 @@ const SignUpPage = () => {
       <h2>회원가입</h2>
       <form onSubmit={handleSubmitSignUp}>
         <div>
-          <label htmlFor='email'>Email:</label>
+          <label htmlFor="email">Email:</label>
           <input
-            id='email'
-            type='email'
+            id="email"
+            type="email"
             value={email}
-            placeholder='ex. changedays@gmail.com'
+            placeholder="ex. changedays@gmail.com"
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
         <div>
-          <label htmlFor='password'>Password:</label>
+          <label htmlFor="password">Password:</label>
           <input
-            id='password'
-            type='password'
+            id="password"
+            type="password"
             value={password}
-            placeholder='ex. changedays!1234'
+            placeholder="ex. changedays!1234"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
         <div>
-          <button type='submit'>회원가입하기</button>
+          <button type="submit">회원가입하기</button>
         </div>
       </form>
     </div>
