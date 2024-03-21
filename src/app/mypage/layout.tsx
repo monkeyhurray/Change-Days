4ba16eb47e644c1705e0ec942845ae7b8e32f4fc
Dahyeon Jin <wlsekgus12@gmail.com>
2024-03-22 03:57:27 +0900

    필요없는 주석 제거

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -21,7 +21,6 @@ const MyPageLayout = ({ children }: { children: React.ReactNode }) => {
           router.push(`/mypage/${data.session?.user.id}`);
           setShouldRender(true);
         } else {
-          // alert가 의존성 배열 때문에 2번 뜨는 이슈가 있음 => 해결해야 함
           alert("로그인이 필요한 서비스입니다. 로그인 페이지로 이동합니다.");
           router.replace("/sign/signin");
           return;
