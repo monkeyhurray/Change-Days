f5ca6a510068763b80df5a59a9b17e56fcfc0072
rlaeodus <gwg03185@gmail.com>
2024-03-21 12:25:17 +0900

    풀땡기기전 커밋

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -12,7 +12,8 @@ const SearchPage = () => {
   const [searchClicked, setSearchClicked] = useState(false);
   const [debouncedSearchItem, setDebouncedSearchItem] = useState("");
   const { searchText } = useSearchStore((state) => state);
-  console.log("searchText", searchText);
+  const resetText = useSearchStore((state) => state.resetText);
+
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setDebouncedSearchItem(searchItem);
@@ -21,35 +22,39 @@ const SearchPage = () => {
     return () => clearTimeout(timeoutId);
   }, [searchItem]);
 
-  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } =
-    useInfiniteQuery({
-      queryKey: ["challenges", debouncedSearchItem],
-      queryFn: async ({ pageParam }) => {
-        let supabaseData = supabase
-          .from("challenges")
-          .select("*")
-          .order("created_at");
-        if (searchClicked === false) {
-          supabaseData = supabaseData.ilike("name", `%${searchItem}%`);
-        }
-        if (searchText) {
-          supabaseData = supabaseData.ilike("name", `%${searchText}%`);
-        }
-        const { data, error } = await supabaseData.range(
-          (pageParam - 1) * 10,
-          pageParam * 10 - 1
-        );
+  const {
+    data: infiniteData,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["challenges", debouncedSearchItem],
+    queryFn: async ({ pageParam }: { pageParam: number }) => {
+      let supabaseTableData = supabase
+        .from("challenges")
+        .select("*")
+        .order("created_at");
+      if (searchClicked === false) {
+        supabaseTableData = supabaseTableData.ilike("name", `%${searchItem}%`);
+      }
+      if (searchText) {
+        supabaseTableData = supabaseTableData.ilike("name", `%${searchText}%`);
+      }
+      const { data: supabaseData, error } = await supabaseTableData.range(
+        (pageParam - 1) * 10,
+        pageParam * 10 - 1
+      );
 
-        if (error) {
-          throw new Error(error.message);
-        }
+      if (error) {
+        throw new Error(error.message);
+      }
 
-        return { data, nextPage: pageParam + 1 };
-      },
-      initialPageParam: 1,
-      getNextPageParam: (lastPage) => lastPage.nextPage,
-    });
-  console.log("data", data);
+      return { supabaseData, nextPage: pageParam + 1 };
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
+  });
+  console.log("infiniteData", infiniteData);
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
@@ -71,14 +76,16 @@ const SearchPage = () => {
       behavior: "smooth",
     });
   };
-  console.log("searchClicked", searchClicked);
+
+  // const content = infiniteData?.pages.map((page) => page.supabaseData);
+  useEffect(() => {
+    resetText();
+  }, []);
   return (
-    <div className="bg-white flex justify-center min-h-full py-16">
-      <div className="max-w-md text-center">
-        <form
-          className="flex items-center justify-center mb-8"
-          // onSubmit={handleSearchSubmit}
-        >
+    <div className="bg-white flex justify-center min-h-full py-32">
+      {/* <div className="max-w-md text-center"> */}
+      <div className="w-full text-center">
+        <form className="flex items-center justify-center mb-8">
           <input
             type="text"
             placeholder="검색어를 입력하세요"
@@ -88,28 +95,36 @@ const SearchPage = () => {
             className="px-4 py-2 mr-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
           />
         </form>
-        <div className="flex flex-col">
-          {data?.pages.map((page) =>
-            page.data.map((challenge) => (
-              <ChallengeCard
-                key={challenge.id}
-                challenge={challenge}
-                innerRef={ref}
-              />
-            ))
-          )}
-          {isFetchingNextPage ? (
-            <h3 ref={ref}>무한 스크롤 로딩...</h3>
-          ) : (
-            <h3 ref={ref} />
-          )}
+        <div className="flex p-4  pt-20 max-h-full justify-center">
+          <div className="flex flex-wrap  gap-8 justify-center">
+            {infiniteData?.pages.map((page) => page.supabaseData)[0].length !==
+            0 ? (
+              infiniteData?.pages.map((page) =>
+                page.supabaseData.map((challenge) => (
+                  <ChallengeCard
+                    key={challenge.id}
+                    challenge={challenge}
+                    innerRef={ref}
+                  />
+                ))
+              )
+            ) : (
+              <h3>검색된 데이터가 없습니다.</h3>
+            )}
+          </div>
+
+          <button
+            onClick={scrollToTop}
+            className="fixed bottom-10 right-10 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            맨 위로
+          </button>
         </div>
-        <button
-          onClick={scrollToTop}
-          className="fixed bottom-10 right-10 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-        >
-          맨 위로
-        </button>
+        {isFetchingNextPage && (
+          <div className="flex flex-wrap text-center justify-center">
+            로딩중입니다...
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/ChallengeCard.tsx
+++ src/components/ChallengeCard.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { FC } from "react";
+// import publicChallengeImage from "../../public/challenge.jpeg";
 
 // extends를 해주면 <TodoCard 뒤에 속성들 나타남
 interface ChallengeCardProps
@@ -12,14 +14,23 @@ const ChallengeCard: FC<ChallengeCardProps> = ({ challenge, innerRef }) => {
 
   return (
     <div
-      className="bg-red-500  p-4 text-2xl border-2 flex flex-col"
+      className="bg-red-500  p-4 text-2xl border-2 flex flex-col cursor-pointer hover:scale-105"
       key={challenge.id}
       ref={innerRef}
       onClick={() => {
         router.push(`/challenge/${challenge.id}`);
       }}
     >
-      <section>id:{challenge.id}</section>
+      {/* <Image src={publicChallengeImage} className="w-full" alt="챌린지" /> */}
+      {/* <Image src={challenge.thumbnail} className="w-16" alt="챌린지" /> */}
+      {/* <Image
+        src={challenge.thumbnail}
+        width={300}
+        height={200}
+        className="w-16"
+        alt="챌린지"
+      /> */}
+      {/* <section>id:{challenge.id}</section> */}
       <section>name:{challenge.name}</section>
       <section>etc:{challenge.etc}</section>
       <section>period:{challenge.period}</section>
