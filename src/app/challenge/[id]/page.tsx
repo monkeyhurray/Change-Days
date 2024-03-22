e93eaa0a30a481ebd17c508ab33ec22538c503d3
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2024-03-22 18:44:45 +0900

    챌린지 페이지에서 작성자 id 작성자 name으로 변경

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -1,59 +1,96 @@
+"use client";
 import { supabase } from "@/supabase/supabase";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChallengeListRow } from "@/app/page";
 import ParticipateBtn from "@/components/challenge/ParticipateBtn";
 
-type props = {
+type Props = {
   params: { id: string };
 };
-const ChallengePage = async ({ params }: props) => {
+
+const ChallengePage = ({ params }: Props) => {
   const id = params.id;
+  const [challenge, setChallenge] = useState<ChallengeListRow | null>(null);
+  const [createdByUser, setCreatedByUser] = useState<string | null>(null);
 
-  const { data, error } = await supabase
-    .from<any, ChallengeListRow>('challenges')
-    .select('*')
-    .eq('id', id);
+  useEffect(() => {
+    const fetchData = async () => {
+      // 챌린지 정보 가져오기
+      const { data: challengeData, error: challengeError } = await supabase
+        .from("challenges")
+        .select("*")
+        .eq("id", id)
+        .single();
 
-  if (error) {
-    console.log(error.message);
+      if (challengeError) {
+        console.error(
+          "챌린지 정보를 가져오는 중 오류가 발생했습니다.",
+          challengeError
+        );
+        return;
+      }
+
+      setChallenge(challengeData);
+
+      // 사용자 정보 가져오기
+      if (challengeData && challengeData.created_by) {
+        const { data: userData, error: userError } = await supabase
+          .from("users")
+          .select("*")
+          .eq("uid", challengeData.created_by)
+          .single();
+
+        if (userError) {
+          console.error(
+            "사용자 정보를 가져오는 중 오류가 발생했습니다.",
+            userError
+          );
+          return;
+        }
+
+        setCreatedByUser(userData?.name || null);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  if (!challenge) {
+    return <div>챌린지 정보를 불러오는 중입니다...</div>;
   }
 
-  if (data && data.length > 0) {
-    const challenge: ChallengeListRow = data[0] as ChallengeListRow;
-    return (
-      <>
-        <div>
-          <div className="border-b-2 border-b-gray-300 py-3">
-            <p className="mb-2">
-              <span className="mr-5 text-2xl">{challenge.name}</span>
-              <span className="mr-3 text-white bg-gray-600 p-2 rounded-xl">
-                {challenge.frequency}
-              </span>
-              <span className="mr-3 text-white bg-gray-600 p-2 rounded-xl">
-                {challenge.start_date}-{challenge.end_date}
-              </span>
-            </p>
-            <span>작성자 : {challenge.created_by}</span>
-          </div>
-          <div>
-            <p className="py-5 text-xl font-bold">챌린지 소개</p>
-            <div className="h-36">{challenge.etc}</div>
-          </div>
-          <div>
-            <p className="py-5 text-xl font-bold">인증 방법</p>
-            <div className="h-36">사진을 예쁘게 찍어서 올리세요</div>
-
-          </div>
-          <div>
-            <p className='py-5 '>
-              참가자 수:<span>1234명</span>
-            </p>
-          </div>
-          <ParticipateBtn challengeId={id} />
+  return (
+    <>
+      <div>
+        <div className="border-b-2 border-b-gray-300 py-3">
+          <p className="mb-2">
+            <span className="mr-5 text-2xl">{challenge.name}</span>
+            <span className="mr-3 text-white bg-gray-600 p-2 rounded-xl">
+              {challenge.frequency}
+            </span>
+            <span className="mr-3 text-white bg-gray-600 p-2 rounded-xl">
+              {challenge.start_date}-{challenge.end_date}
+            </span>
+          </p>
+          <span>작성자 : {createdByUser}</span> {/* 작성자 표시 */}
         </div>
-      </>
-    );
-  }
+        <div>
+          <p className="py-5 text-xl font-bold">챌린지 소개</p>
+          <div className="h-36">{challenge.etc}</div>
+        </div>
+        <div>
+          <p className="py-5 text-xl font-bold">인증 방법</p>
+          <div className="h-36">사진을 예쁘게 찍어서 올리세요</div>
+        </div>
+        <div>
+          <p className="py-5 ">
+            참가자 수:<span>1234명</span>
+          </p>
+        </div>
+        <ParticipateBtn challengeId={id} />
+      </div>
+    </>
+  );
 };
 
 export default ChallengePage;
