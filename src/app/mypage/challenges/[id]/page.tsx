22a3194581a5f7a301fd78d086da3d4f4b006195
gidalim <gidalimforsomeday@gmail.com>
2024-03-24 21:34:58 +0900

    [ADD] create fulfill_check date button

--- src/app/mypage/challenges/[id]/page.tsx
+++ src/app/mypage/challenges/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { supabase } from "@/supabase/supabase";
 import { useParams, useRouter } from "next/navigation"
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 
 const ChallengePage = () => {
@@ -45,6 +45,50 @@ const ChallengePage = () => {
     fetchFulfillments();
   }, [challengeId]);
 
+  const handleFileUpload = async (e :ChangeEvent<HTMLInputElement> ) => {
+    const file = e.target.files![0];
+    if (!file) return;
+
+    const { error, data } = await supabase.storage.from('images').upload(`challenge/${file.name}`, file);
+
+    if (error) {
+      alert(`업로드 실패: ${error.message}`);
+    } else {
+      alert('업로드 성공!');
+
+      const { data: userChallengeData, error: userChallengeError } = await supabase
+      .from('user_challenges')
+      .select('id')
+      .eq('challenge_id', challengeId)
+        .single();
+      
+      if (userChallengeError) {
+      
+      console.error(userChallengeError);
+      return;
+    }
+
+    const {error: fulfillError } = await supabase
+      .from('user_fulfill')
+      .insert([
+        { 
+          user_challenge_id: userChallengeData.id, 
+          date: new Date().toISOString(), 
+          isdone: true, 
+          
+        }
+
+      ]);
+      console.log('ddds',userChallengeData)
+
+    if (fulfillError) {
+      console.error(fulfillError);
+      return;
+    }
+        
+      }
+    };
+
 
   const handleButtonClick = () => {
     router.back()
@@ -65,6 +109,9 @@ const ChallengePage = () => {
           <p>아직 완료된 챌린지가 없습니다.</p>
         )}
       </ul>
+    </div>
+    <div>
+      <input type="file" onChange={handleFileUpload}></input>
     </div>
 
   </div>
