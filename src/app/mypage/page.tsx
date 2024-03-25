ce1d666b95b81454774fe3697e3d2af88f500844
sangho <keumm9635@gmail.com>
2024-03-25 10:23:41 +0900

    Fix: mypage 프로필사진 업데이트 기능 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { UserDataProps } from '@/components/mypage/UserData';
-import { supabase } from '@/supabase/supabase';
-import Link from 'next/link';
-import { FaChevronRight } from 'react-icons/fa';
-import bonobo from '../../../public/bonobo.jpeg';
-import Image from 'next/image';
-import { fetchChallenges } from '@/utils/fetchChallenges';
+import React, { useEffect, useState } from "react";
+import { UserDataProps } from "@/components/mypage/UserData";
+import { supabase } from "@/supabase/supabase";
+import Link from "next/link";
+import { FaChevronRight } from "react-icons/fa";
+import bonobo from "../../../public/bonobo.jpeg";
+import Image from "next/image";
+import { fetchChallenges } from "@/utils/fetchChallenges";
 
 type Challenge = {
   id: string;
@@ -32,18 +32,16 @@ const UserPage = () => {
   // const [user, setUser] = useState(null);
   const [challenges, setChallenges] = useState<UserChallenge[]>([]);
 
-  const fetchUserData = async () => {
+  const usefetchUserData = async () => {
     const { data, error } = await supabase.auth.getUser();
-    console.log(data);
 
     if (data.user) {
-      console.log('userId', data.user.id);
       const { data: userData, error: userError } = await supabase
-        .from('users')
-        .select('*')
-        .eq('uid, nickname', data.user.id)
+        .from("users")
+        .select("*")
+        .eq("uid, nickname", data.user.id)
         .single();
-      console.log('user', userData);
+
       setUser(userData);
     }
   };
@@ -70,50 +68,37 @@ const UserPage = () => {
         fetchChallenges(userId, setChallenges);
       }
     });
-    fetchUserData();
-    // fetchChallenges();
-  }, []);
+    usefetchUserData();
+  }, [user?.url]);
 
   if (user) {
     return (
-      <div className='mb-10'>
-        <Link href='/mypage/profile' className='flex items-center mb-5'>
-          <div className=' rounded-full overflow-hidden w-20 h-20 mr-5'>
+      <div className="mb-10">
+        <Link href="/mypage/profile" className="flex items-center mb-5">
+          <div className=" rounded-full overflow-hidden w-20 h-20 mr-5">
             {/* <img src={user.url} alt='userImg' className='w-full h-full' /> */}
-            <Image src={bonobo} alt='유저이미지' width={150} height={150} />
+            <Image
+              src={user.url !== null ? user.url : bonobo}
+              alt="유저이미지"
+              width={150}
+              height={150}
+            />
           </div>
-          <p className='text-2xl mr-3'>{user.nickname}</p>
+          <p className="text-2xl mr-3">{user.nickname}</p>
           <FaChevronRight size={20} />
         </Link>
 
         <div>
-          <h1 className='text-xl mb-5 font-bold mt-10'>챌린지 현황</h1>
-          <nav className='border border-gray-500 rounded-md flex justify-between p-2'>
+          <h1 className="text-xl mb-5 font-bold mt-10">챌린지 현황</h1>
+          <nav className="border border-gray-500 rounded-md flex justify-between p-2">
             <Link
-              className='flex-1 mx-auto text-center'
+              className="flex-1 mx-auto text-center"
               href={`mypage/challenges`}
             >
               <p>진행</p>
               <p>{`${challenges.length}`}</p>
             </Link>
-            {/* <Link
-              className='flex-1 border-r border-r-gray-500 mx-auto text-center'
-              href={`mypage/challenges`}
-            >
-              <p>완료</p>
-              <p>1</p>
-            </Link>
-            <Link
-              className='flex-1 mx-auto text-center'
-              href={`mypage/challenges`}
-            >
-              <p>개설</p>
-              <p>1</p>
-            </Link> */}
           </nav>
-          {/* <section className="flex justify-center gap-8">
-            <p>현재 유저의 챌린지</p>
-          </section> */}
         </div>
       </div>
     );
--- src/components/mypage/UserData.tsx
+++ src/components/mypage/UserData.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import bonobo from '../../../public/bonobo.jpeg';
+import React from "react";
+import bonobo from "../../../public/bonobo.jpeg";
 export type UserDataProps = {
   name: string;
   bonobo: File;
   nickname: string;
   email: string;
+  url: string;
 };
 
 const UserData: React.FC<UserDataProps> = () => {
   return (
     <div>
-      <figure className='flex flex-col items-center gap-8 mt-8'>
-        <img src={bonobo} alt='유저이미지' width={150} height={150} />
-        <p>{name || '유저이름'}</p>
+      <figure className="flex flex-col items-center gap-8 mt-8">
+        <img src={bonobo} alt="유저이미지" width={150} height={150} />
+        <p>{name || "유저이름"}</p>
       </figure>
     </div>
   );
